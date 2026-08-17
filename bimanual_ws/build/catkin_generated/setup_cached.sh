db74704455be5dfa0f1c618ac1662dff427424d9
Jonghyun Kim [sc23j3k] <sc23j3k@uol-bcpqk000685.leeds.ac.uk>
2026-08-17 17:25:53 +0100

    deleted duplicate files and added mjctrl

--- bimanual_ws/build/catkin_generated/setup_cached.sh
+++ bimanual_ws/build/catkin_generated/setup_cached.sh
@@ -8,6 +8,9 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/uolstore/home/users/sc23j3k/bimanual_control/bimanual_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/uolstore/home/users/sc23j3k/bimanual_control/bimanual_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/uolstore/home/users/sc23j3k/bimanual_control/bimanual_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/uolstore/home/users/sc23j3k/bimanual_control/bimanual_ws/build'
+export PYTHONPATH="/uolstore/home/users/sc23j3k/bimanual_control/bimanual_ws/devel/lib/python3/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES='/uolstore/home/users/sc23j3k/bimanual_control/bimanual_ws/devel/share/common-lisp'
 export ROS_PACKAGE_PATH="/uolstore/home/users/sc23j3k/bimanual_control/bimanual_ws/src:$ROS_PACKAGE_PATH"
---
+++ bimanual_ws/build/ros_control/controller_manager/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/uolstore/home/users/sc23j3k/bimanual_control/bimanual_ws/src/ros_control/controller_manager/include".split(';') if "/uolstore/home/users/sc23j3k/bimanual_control/bimanual_ws/src/ros_control/controller_manager/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "controller_interface;controller_manager_msgs;hardware_interface;pluginlib;roscpp".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lcontroller_manager".split(';') if "-lcontroller_manager" != "" else []
+PROJECT_NAME = "controller_manager"
+PROJECT_SPACE_DIR = "/uolstore/home/users/sc23j3k/bimanual_control/bimanual_ws/devel"
+PROJECT_VERSION = "0.20.0"
